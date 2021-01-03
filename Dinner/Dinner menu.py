d9e1298fcfaffa69ae2eb656c17e4a4d28658a33
linndaaa <72157419+linndaaa@users.noreply.github.com>
2021-01-03 23:52:33 +0800

    Add files via upload

---
+++ Dinner/Dinner menu.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  8 21:00:08 2020
+
+@author: linda
+"""
+import Dinner
+
+def menu():
+    menuItem = """Welcome to Main Page:
+        Do input the relevant information.
+        1. Check availability 
+        2. Make reservation 
+        X. End application
+        Enter your choice: 
+        """
+        
+    while True:
+        choice = input(menuItem)
+        if choice == '1':
+            Dinner.availability()
+        elif choice == '2':
+            Dinner.Cust_details()           
+        elif choice.upper() == 'X':
+            
+            break
+
+menu()
